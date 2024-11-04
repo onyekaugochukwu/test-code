22ecbbb3ad1919b3efe76c8ff8e738a701024e6b
Ugochukwu Onyeka <92909501+onyekaugochukwu@users.noreply.github.com>
2024-11-04 22:56:20 +0000

    updates to demo

---
+++ init.sql
@@ -0,0 +1,13 @@
+CREATE TABLE students (
+    id TEXT,
+    firstname TEXT,
+    lastname TEXT,
+    course TEXT
+);
+
+INSERT INTO students (id, firstname, lastname, course)
+VALUES 
+('1', 'Michael', 'West', 'SRE Fundamentals with Google'),
+('2', 'James', 'Corban', 'SRE Fundamentals with Google'),
+('3', 'Sarah', 'Johnson', 'SRE Fundamentals with Google'),
+('4', 'Alex', 'Smith', 'SRE Fundamentals with Google');
